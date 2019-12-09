36d1b6cfa8f879da38df1618be2ac666e71c0ac7
dnstankiewicz <58120609+dnstankiewicz@users.noreply.github.com>
2019-12-09 18:15:23 +0100

    Fixed version of 3rd homework

--- script.js
+++ script.js
@@ -1,43 +1,73 @@
-const concertButtons = [...document.querySelectorAll('.concerts__button')];
-const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('.page-navigation');
-const navItems = [...document.querySelectorAll('.page-navigation__item')];
-const footer = document.querySelector('.page-footer');
-const form = document.querySelector('.form');
+/*
+######################################
+HAMBURGER MENU
+######################################
+*/
+
+const hamburgerMenu = document.querySelector('.hamburger');
+const navigationPanel = document.querySelector('.page-navigation');
+const navigationLinks = document.querySelectorAll('.page-navigation__item');
 
 
-const menuAction = () => {
-    hamburger.classList.toggle('hamburger--active');
-    nav.classList.toggle('page-navigation--active');
-    footer.classList.toggle('page-footer--active');
+const toggleHamburgerMenu = () => {
+    navigationPanel.classList.toggle('page-navigation--active');
 };
 
+hamburgerMenu.addEventListener('click', toggleHamburgerMenu);
+navigationLinks.forEach(
+    navigationSingleLink => {
+        navigationSingleLink.addEventListener('click',toggleHamburgerMenu);
+    }
+)
 
-hamburger.addEventListener('click', menuAction);
 
-for (let i = 0; i < navItems.length; i++) {
-    navItems[i].addEventListener('click', menuAction);
-};
+/*
+######################################
+ZMIANA PRZYCISKU
+######################################
+*/
+
+const ticketAllButtons = document.querySelectorAll('.concerts__button');
+
+ticketAllButtons.forEach(
+        ticketSingleButton => {
+            ticketSingleButton.addEventListener('click', () => {
+                ticketSingleButton.style.pointerEvents = 'none';
+                ticketSingleButton.style.border = 'none';
+                ticketSingleButton.style.backgroundColor = 'transparent';
+                ticketSingleButton.textContent = 'Have fun!';        
+            })
+        }
+);
 
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+/*
+######################################
+FORMULARZ DO KONSOLI
+######################################
+*/
 
+const form = document.querySelector('.contact__form');
+
+form.addEventListener('submit', (Event) => {
+
+    // zabezpieczenie przed wysłaniem danych i przeladowaniem strony
+    Event.preventDefault(); 
+    
+    //odczyt danych z forma
     const formData = new FormData(form);
-    const inputs = [...document.querySelectorAll('.form__input')];
-
     console.log(Object.fromEntries(formData));
-
-    inputs.forEach(input => {
-        input.value = '';
-    });
-})
-
-
-concertButtons.forEach(concertButton => {
-    concertButton.addEventListener('click', () => {
-        concertButton.textContent = 'have fun!';
-        concertButton.style.backgroundColor = 'transparent';
-        concertButton.style.pointerEvents = 'none';
-    })
+    
+    // wyzerowanie wartości formularza na stronie
+    const formInputValues = document.querySelectorAll('.form__input');
+    formInputValues[0].value= null;
+    formInputValues[1].value= null;
 });
+
+
+
+
+
+
+
+
